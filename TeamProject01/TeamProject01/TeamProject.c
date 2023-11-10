a55ea87325ebe525905eca40894f47b859b740a7
starofmars06 <82389930+starofmars06@users.noreply.github.com>
2023-11-10 23:45:49 +0900

    bmi 출력(cmd 창) 추가
    기존 운동종목 선택, 이름 기능은 유지하고 있습니다.

--- TeamProject01/TeamProject01/TeamProject.c
+++ TeamProject01/TeamProject01/TeamProject.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BLUE     "\x1b[34m"
+#define ANSI_COLOR_MAGENTA  "\x1b[35m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
 char name[20];
 
 char* save_load_name() {
@@ -61,6 +68,133 @@ char* save_load_name() {
 	}
 
 	return name;
+}
+
+void bmi_calculation(char* name) {
+	int height, weight;
+	float fatness;      //비만도
+
+	//신체 정보 입력(키, 몸무게)
+	printf("\n*******************\n");            //구분선
+	printf("반갑습니다, %s님!\n", name);            //환영문구
+	printf("신체 정보를 입력해주세요.\n");
+
+	printf("키는 얼마입니까?(cm) : ");
+	scanf("%d", &height);
+
+	printf("몸무게는 얼마입니까?(kg) : ");
+	scanf("%d", &weight);
+
+	//----------------------------------------------------------------------------
+	// --> 비만도 계산
+
+	//카우프 지수에 따르면 "비만도" = 체중(kg) / (신장(m) * 신장(m))
+	fatness = (float)weight / ((float)height * 0.01 * (float)height * 0.01);   //미터값을 기준으로 하기 때문에 키에 각각 0.01을 곱해줌.
+
+	//18.5 이하 저체중, 18.5~23.0 정상, 23.0~25.0 과체중, 25.0~30.0 비만, 30.0 이상 비만
+	if (fatness < 18.5) {
+		printf(ANSI_COLOR_YELLOW ".........□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".........□□□□\n");
+		printf("........□□□□□□□\n");
+		printf("......□□□□□□□□□□\n");
+		printf("....□□□□□□□□□□□□□□\n");
+		printf("...□□   □□□□□□   □□\n");
+		printf("..□□    □□□□□□    □□\n");
+		printf("........□□□□□□\n");
+		printf("........□□  □□\n");
+		printf("........□□  □□\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf(".........<저체중입니다.건강에 유의해주세요.>\n");
+
+		printf("\n체중 증량을 위해 [근육 증량] 운동을 권장합니다.\n" ANSI_COLOR_RESET);
+	}
+	else if (fatness < 23.0) {
+		printf(ANSI_COLOR_GREEN ".........□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".........□□□□\n");
+		printf("........□□□□□□□\n");
+		printf("......□□□□□□□□□□\n");
+		printf("....□□□□□□□□□□□□□□\n");
+		printf("...□□   □□□□□□   □□\n");
+		printf("..□□    □□□□□□    □□\n");
+		printf("........■■■■■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+
+		printf(".........<정상체중입니다>\n" ANSI_COLOR_RESET);
+	}
+	else if (fatness < 25.0) {
+		printf(ANSI_COLOR_BLUE ".........□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".........□□□□\n");
+		printf("........□□□□□□□\n");
+		printf("......■■■■■■■■■■\n");
+		printf("....■■■■■■■■■■■■■■\n");
+		printf("...■■   ■■■■■■   ■■\n");
+		printf("..■■    ■■■■■■    ■■\n");
+		printf("........■■■■■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+
+		printf(".........<과체중입니다. 건강에 유의해주세요.>\n");
+
+		printf("\n체중 감량을 위해 [체지방 감소] 운동을 권장합니다.\n" ANSI_COLOR_RESET);
+	}
+	else if (fatness < 30.0) {
+		printf(ANSI_COLOR_MAGENTA ".........□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".......□□□□□□□□\n");
+		printf(".........■■■■\n");
+		printf("........■■■■■■■\n");
+		printf("......■■■■■■■■■■\n");
+		printf("....■■■■■■■■■■■■■■\n");
+		printf("...■■   ■■■■■■   ■■\n");
+		printf("..■■    ■■■■■■    ■■\n");
+		printf("........■■■■■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+
+		printf(".........<비만입니다. 건강에 유의해주세요.>\n");
+
+		printf("\n체중 감량을 위해 [체지방 감소] 운동을 권장합니다.\n" ANSI_COLOR_RESET);
+	}
+	else {
+		printf(ANSI_COLOR_RED ".........■■■■\n");
+		printf(".......■■■■■■■■\n");
+		printf(".......■■■■■■■■\n");
+		printf(".........■■■■\n");
+		printf("........■■■■■■■\n");
+		printf("......■■■■■■■■■■\n");
+		printf("....■■■■■■■■■■■■■■\n");
+		printf("...■■   ■■■■■■   ■■\n");
+		printf("..■■    ■■■■■■    ■■\n");
+		printf("........■■■■■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+		printf("........■■  ■■\n");
+
+		printf(".........<고도비만입니다. 건강에 유의해주세요.>\n");
+
+		printf("\n체중 감량을 위해 [체지방 감소] 운동을 권장합니다.\n" ANSI_COLOR_RESET);
+	}
+
 }
 
 void daily_routine(char* name) {
@@ -251,6 +385,7 @@ int main() {
 	save_load_name();
 
 	//2. BMI 측정(신체 정보 입력)
+	bmi_calculation(name);
 
 	//3. 운동 종목 선택하기
 	daily_routine(name);
